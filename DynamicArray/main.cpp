7a052a7a31c1f1335287a2ea518b20b7b539547c
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2025-08-12 01:45:40 -0600

    Add author name to main function

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "menu.cpp"
 
+/*
+    Aguirre Mares Juan Antonio - Ingenieria Informatica
+    Universidad de Guadalajara
+    20/04/2025
+*/
+
 using namespace std;
 
 int main() {
--- SinglyLinkedList/main.cpp
+++ SinglyLinkedList/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "Menu.cpp"
 
+/*
+    Aguirre Mares Juan Antonio - Ingenieria Informatica
+    Universidad de Guadalajara
+    27/04/2025
+*/
+
 using namespace std;
 
 int main() {
